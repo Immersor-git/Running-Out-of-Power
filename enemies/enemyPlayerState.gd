8526bff9e19515e932d81aa4342208d122f0cd1b
Immersor-git <matt.halderman11@gmail.com>
2023-12-29 11:45:10 -0800

    re-added shydog

---
+++ enemies/enemyPlayerState.gd
@@ -0,0 +1,11 @@
+extends Node
+
+#var Player = get_tree().get_root().get_node("Player")
+
+var position = Vector2(0,0)
+var direction = 0
+
+#func _process(delta):
+#	position = Player.global_position
+#	direction = Player.rotation()
+	
