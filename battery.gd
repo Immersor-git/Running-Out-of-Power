862067bb016f083d18052b37c0899fb7cabe1994
Kevin Michael Angel Low <kevinmlowg@yahoo.com>
2023-12-29 16:39:56 -0800

    Interactables work!

--- battery.gd
+++ battery.gd
@@ -15,6 +15,5 @@ func _process(delta):
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		if get_rect().has_point(to_local(get_global_mouse_position())) and position.distance_to(%player.position) < 50:
-			%player.batteries += 1
-			print('DISTANCE', position.distance_to(%player.position))
+			print("clicked")
 			queue_free()
--- text_boxes/text_box.gd
+++ text_boxes/text_box.gd
@@ -2,25 +2,34 @@ extends CanvasLayer
 
 var file_path = "res://text_boxes/dialogue.json"
 var itemData = {}
-@export var showDescription : bool = true
+@export var showDescription : bool = false
 
 
 @onready var textBoxContainer = $TextBoxContainer
 @onready var textBoxText = $TextBoxContainer/MarginContainer/HBoxContainer/LabelText
 
 var iterative_text: int = 0
-var textToAdd = "this text is added and its a little bit kinda long lmfao"
+var textToAdd = "Q text is added and its a little bit kinda long lmfao"
 
 func _physics_process(delta):
-	if Input.is_action_pressed("close_menu") || Input.is_action_pressed("click"):
+	if Input.is_action_pressed("close_menu"):
+		showDescription = false
+		iterative_text = 0
 		close_textbox()
 	#if Input.is_action_pressed("")
 
 
 func _ready():
 	close_textbox()
+	#if showDescription:
+		#textToAdd = _load_json_file(file_path, "Motherboard")
+		#print(textToAdd[0])
+		#add_text(textToAdd[0])
+
+func showBox(interactObject : String):
+	close_textbox()
 	if showDescription:
-		textToAdd = _load_json_file(file_path, "Motherboard")
+		textToAdd = _load_json_file(file_path, interactObject)
 		print(textToAdd[0])
 		add_text(textToAdd[0])
 
@@ -45,7 +54,7 @@ func _load_scene_text():
 	pass
 		
 func close_textbox():
-	textBoxText.text = ""
+	#textBoxText.text = ""
 	textBoxContainer.hide()
 
 func show_textbox():
@@ -62,10 +71,13 @@ func add_text(next_text):
 
 func _on_character_timeout():
 	#var mainString = textBoxText.get_text()
-	iterative_text += 1
 	print(iterative_text)
-	textBoxText.text = textToAdd[0].substr(0, iterative_text)
-	if iterative_text == textToAdd[0].length():
-		$TextBoxContainer/MarginContainer/HBoxContainer/nextChar.stop()
+	if showDescription:
+		iterative_text += 1
+		print(iterative_text)
+		textBoxText.text = textToAdd[0].substr(0, iterative_text)
+		#if iterative_text == textToAdd[0].length():
+			#$TextBoxContainer/MarginContainer/HBoxContainer/nextChar.stop()
+			#iterative_text = 0
 		print("text typing done!")
-	 # Replace with function body.
+
